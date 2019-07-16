d65a191160c61495e9d2c26e11df66f72b8f2970
Xavier Carpentier <xcapetir@gmail.com>
2019-07-16 08:37:28 +0200

    fix scroll

--- src/GiftedChat.tsx
+++ src/GiftedChat.tsx
@@ -651,7 +651,7 @@ class GiftedChat<TMessage extends IMessage = IMessage> extends React.Component<
       this.props.onSend(newMessages)
     }
 
-    setTimeout(() => this.scrollToBottom(), 100)
+    setTimeout(() => this.scrollToBottom(false), 100)
 
     if (shouldResetInputToolbar === true) {
       setTimeout(() => {
@@ -666,7 +666,7 @@ class GiftedChat<TMessage extends IMessage = IMessage> extends React.Component<
     if (this.props.onQuickReply) {
       this.props.onQuickReply(replies)
     }
-    setTimeout(() => this.scrollToBottom(), 100)
+    setTimeout(() => this.scrollToBottom(false), 100)
   }
 
   resetInputToolbar() {
